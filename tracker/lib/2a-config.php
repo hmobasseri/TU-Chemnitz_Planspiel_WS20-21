3e254e121158fb005d58d46fca84d832a12bb4ea
hadi <hadimobasseri@gmail.com>
2020-12-21 12:45:46 +0100

    This is first version of gps-tracker project

---
+++ tracker/lib/2a-config.php
@@ -0,0 +1,14 @@
+<?php
+// MUTE NOTICES
+error_reporting(E_ALL & ~E_NOTICE);
+
+// DATABASE SETTINGS - CHANGE THESE TO YOUR OWN
+define('DB_HOST', '62.108.32.157');
+define('DB_NAME', 'pgjbludp_gpstracker');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'pgjbludp_root');
+define('DB_PASSWORD', 'Ic5o10t?');
+
+// AUTO FILE PATH
+define('PATH_LIB', __DIR__ . DIRECTORY_SEPARATOR);
+?>
